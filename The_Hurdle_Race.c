ee2b4ae9f48238840621e91aac931f055964a9aa
Abdelqoddous01 <144550050+Abdelqoddous01@users.noreply.github.com>
2023-11-04 00:04:49 +0100

    Add files via upload

---
+++ The_Hurdle_Race.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+int main(){
+    int n,k;
+    scanf("%d%d",&n,&k);
+    int max=INT_MIN;
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&x);
+        if (x>max)
+        {
+            max=x;
+        }
+    }
+    if (max-k>0)
+    {
+        printf("%d",max-k);
+    }
+    else{
+        printf("0");
+    }
+    return 0;
+}
