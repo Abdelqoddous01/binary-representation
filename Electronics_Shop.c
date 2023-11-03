197f9a2402c895beab5d93295c5a2ff5424a7861
Abdelqoddous01 <144550050+Abdelqoddous01@users.noreply.github.com>
2023-11-03 23:38:49 +0100

    Add files via upload

---
+++ Electronics_Shop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+    int b,n,m;
+    scanf("%d%d%d",&b,&n,&m);
+    int ak[n],au[m];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&ak[i]);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d",&au[i]);
+    }
+    int max=-1;
+    for (int i = 0; i < n; i++){
+    
+        for (int j = 0; j < m; j++)
+        {
+            if (ak[i]+au[j]>max && ak[i]+au[j]<=b)
+            {
+                max=ak[i]+au[j];
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
